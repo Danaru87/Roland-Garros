f62e312d61905f485d052caf19bb037cb4fdb6b6
Arnaud Da Silva <dasilva.arnaud@gmail.com>
2015-02-13 10:47:37 +0100

    Clean

--- application/controllers/joueur.php
+++ application/controllers/joueur.php
@@ -27,9 +27,9 @@ class Joueur extends REST_Controller
         // Configure limits on our controller methods. Ensure
         // you have created the 'limits' table and enabled 'limits'
         // within application/config/rest.php
-        $this->methods['user_get']['limit'] = 500; //500 requests per hour per user/key
-        $this->methods['user_post']['limit'] = 100; //100 requests per hour per user/key
-        $this->methods['user_delete']['limit'] = 50; //50 requests per hour per user/key
+        //$this->methods['user_get']['limit'] = 500; //500 requests per hour per user/key
+        //$this->methods['user_post']['limit'] = 100; //100 requests per hour per user/key
+        //$this->methods['user_delete']['limit'] = 50; //50 requests per hour per user/key
     }
     
     /* 
